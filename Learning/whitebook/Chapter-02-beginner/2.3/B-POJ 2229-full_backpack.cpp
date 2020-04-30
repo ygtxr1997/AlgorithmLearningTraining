8e65f1bfb8356bb05881224157f037b0cafdaaf8
yuange <gavingreenson@icloud.com>
2020-04-30 14:47:19 +0800

    update POJ2229

--- Learning/whitebook/Chapter-02-beginner/2.3/B-POJ 2229-full_backpack.cpp
+++ Learning/whitebook/Chapter-02-beginner/2.3/B-POJ 2229-full_backpack.cpp
@@ -6,23 +6,21 @@ const int maxn = 1000010; // 1000010
 const int mod = 1e9;
 typedef long long ll;
 
-// 完全背包问题, 会TLE
+// 完全背包问题, 会TLE, 剪枝后1016ms擦线过
 // dp[i][j]:前i种物品组成j的总方案数, 使用滚动数组优化
 // dp[i + 1][j] = dp[i][j] + dp[i + 1][j - v[i]]
 // dp[j] = dp[j] + dp[j - v[i]];
 int N;
-ll dp[maxn]; // dp[i][j]:前i种物品组成j的总方案数, 使用滚动数组优化
-ll v[30];
+int h; // 最高位
+int dp[maxn]; // dp[i][j]:前i种物品组成j的总方案数, 使用滚动数组优化
+int v[30];
 int solve(void) {
     memset(dp, 0, sizeof(dp));
-    for (int i = 0; i < 30; ++i) {
-        v[i] = (1 << i); // 每种物品的价值都是2的幂
-    }
     dp[0] = 1;
-    for (int i = 0; i < 30; ++i) {
-        for (int j = 1; j <= N; ++j) {
-            if (j >= v[i]) dp[j] = dp[j] + dp[j - v[i]];
-            else dp[j] = dp[j];
+    for (int i = 0; i < h; ++i) {
+        if (v[i] > N) break; // 更大的就不用考虑了
+        for (int j = v[i]; j <= N; ++j) { // 从v[i]开始遍历也会少许优化
+            dp[j] = dp[j] + dp[j - v[i]];
             dp[j] %= mod;
         }
     }
@@ -30,6 +28,10 @@ int solve(void) {
 }
 
 int main(void) {
+    for (int i = 0; i < 30; ++i) { // 初始化
+        v[i] = (1 << i); // 每种物品的价值都是2的幂
+        if (v[i] >= maxn) h = i;
+    }
     while (scanf("%d", &N) != EOF) {
         printf("%d\n", solve());
     }
